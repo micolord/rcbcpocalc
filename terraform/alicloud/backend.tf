7f644adadd0b4a6aa6341c59315ff47daa4827e8
Michael Angelo Viola <mico@Michaels-MacBook-Pro.local>
2025-06-04 15:34:44 +0800

    updated code files for RCBC

--- terraform/alicloud/backend.tf
+++ terraform/alicloud/backend.tf
@@ -1,7 +1,7 @@
 // alibaba backend configuration for terraform
 terraform {
   backend "oss" {
-    bucket = "dynamic_env-state-bucket-wemaxxlp"
+    bucket = "dynamic_env-state-bucket-rcbcpoc"
     key   = "dynamic_env-state.tfstate"
     region = "ap-southeast-1"
   }
